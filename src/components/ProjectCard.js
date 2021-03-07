1422d239f9cef574b8a42b44464ae721fc3f16cd
maijsp <spak.flash12@gmail.com>
2021-03-08 01:48:29 +0700

    Edit card size

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,10 +4,10 @@ export default function ProjectCard(props) {
     const link = props.link;
 
     return (
-        <div class="max-w-sm bg-white p-6 rounded-lg shadow-lg font-nunito">
-            <div id="header" className="flex items-center mb-4"> 
+        <div class="max-w-sm h-64 bg-white p-6 rounded-lg shadow-lg font-nunito relative">
+            <div id="header" className="flex items-center mb-4">
                 <div id="header-text" className="leading-5">
-                    <h4 id="name" className="text-2xl font-bold">{props.name}</h4>
+                    <h4 id="name" className="text-2xl font-bold leading-7">{props.name}</h4>
                     <div className="mt-2 flex flex-row flex-wrap justify-start">
                     {
                         props.tech.map((tech, index) => {
@@ -20,12 +20,12 @@ export default function ProjectCard(props) {
                 </div>
             </div>
             <div id="quote">
-                <p className=" text-gray-600 text-base">{props.description}</p>
+                <p className="text-gray-600 text-base">{props.description}</p>
             </div>
-            <div className="mt-2" id="link items-center">
+            <div className="" id="link">
                 {
                     link !== "" 
-                    ?   <button className="text-blue-600 text-sm hover:bg-blue-100 rounded-md p-2 transition duration-500 ease-in-out">
+                    ?   <button className="absolute mb-3 bottom-0 text-blue-600 text-sm hover:bg-blue-100 rounded-md p-2 transition duration-500 ease-in-out">
                             <a className=""href={props.link}>Read more</a>
                             <svg className="ml-1 w-4 h-4 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                         </button> 
